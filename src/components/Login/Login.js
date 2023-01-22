39dbbfe7b0147bb83de0e0d52147c9b75ceabbf2
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-21 20:19:25 -0600

    Added useEffect dependencies

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,84 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
+	const [enteredEmail, setEnteredEmail] = useState("");
+	const [emailIsValid, setEmailIsValid] = useState();
+	const [enteredPassword, setEnteredPassword] = useState("");
+	const [passwordIsValid, setPasswordIsValid] = useState();
+	const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+  //So therefore here, we can again import useEffect and then here after our state definitions, we can call useEffect and pass this first function to it and have our array of dependencies.
+	useEffect(() => {
+    //Now, like this, this would only run once and that's when this component first is rendered.
+		setFormIsValid(
+			enteredEmail.includes("@") && enteredPassword.trim().length > 6
+		);
+    //So we have setFormIsValid, enteredEmail and enteredPassword. These are our three things we're using here. So therefore, here, between these brackets, you would add, setFormIsValid, enteredEmail and enteredPassword, just like this. Important for setFormIsValid, you of course don't execute it, that would add the result of this function execution as a dependency, but instead you added like this to add a pointer at this function so that you essentially add the function itself as a dependency.
+  }, [setFormIsValid, enteredEmail, enteredPassword]);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
-  };
+	const emailChangeHandler = (event) => {
+		setEnteredEmail(event.target.value);
+	};
 
-  const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+	const passwordChangeHandler = (event) => {
+		setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
-  };
+		setFormIsValid(
+			event.target.value.trim().length > 6 && enteredEmail.includes("@")
+		);
+	};
 
-  const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
-  };
+	const validateEmailHandler = () => {
+		setEmailIsValid(enteredEmail.includes("@"));
+	};
 
-  const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
-  };
+	const validatePasswordHandler = () => {
+		setPasswordIsValid(enteredPassword.trim().length > 6);
+	};
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
-  };
+	const submitHandler = (event) => {
+		event.preventDefault();
+		props.onLogin(enteredEmail, enteredPassword);
+	};
 
-  return (
-    <Card className={classes.login}>
-      <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={enteredPassword}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
-        <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
-            Login
-          </Button>
-        </div>
-      </form>
-    </Card>
-  );
+	return (
+		<Card className={classes.login}>
+			<form onSubmit={submitHandler}>
+				<div
+					className={`${classes.control} ${
+						emailIsValid === false ? classes.invalid : ""
+					}`}>
+					<label htmlFor="email">E-Mail</label>
+					<input
+						type="email"
+						id="email"
+						value={enteredEmail}
+						onChange={emailChangeHandler}
+						onBlur={validateEmailHandler}
+					/>
+				</div>
+				<div
+					className={`${classes.control} ${
+						passwordIsValid === false ? classes.invalid : ""
+					}`}>
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						id="password"
+						value={enteredPassword}
+						onChange={passwordChangeHandler}
+						onBlur={validatePasswordHandler}
+					/>
+				</div>
+				<div className={classes.actions}>
+					<Button type="submit" className={classes.btn} disabled={!formIsValid}>
+						Login
+					</Button>
+				</div>
+			</form>
+		</Card>
+	);
 };
 
 export default Login;
